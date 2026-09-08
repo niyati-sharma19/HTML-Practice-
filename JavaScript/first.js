df6b731c7e40f4d85f5c4f2dc24b26896cb31f12
Niyati Sharma <niyatisharma676@gmail.com>
2026-09-08 23:27:49 +0530

    Added JavaScript practice files

---
+++ JavaScript/first.js
@@ -0,0 +1,30 @@
+// console.log("Niyati Sharma");
+// console.log("Hello everyone"); 
+Fullname = " Tony Stark";
+console.log(Fullname); 
+x = null ; // empty
+
+y= undefined ; // donot know what is inside 
+console.log(x) ;  
+// Usage of var 
+// it can be redeclared and updates , known as global variable 
+// this is used before 2015
+// it is global scope variable
+var num1 = 45;
+var num1 = 88 ;
+var num3 = 33 ;
+console.log(num1); // 88
+// let variable cannot be re declared but can be updated 
+// it is the good practise 
+// it is block scope variable 
+let a = "yoyo ";
+a = " meow mwow";
+console.log(a); // meow mweow
+//const : it is the variable cannot be re declared & updated
+const l = 99 ;
+//block scope variable
+console.log(l); // 99
+const Pi = 3.14568900;
+let b ;
+// the value it will  print is undefined 
+//const c =  it will produce  an error 
---
+++ JavaScript/firstpractise.js
@@ -0,0 +1,17 @@
+// Create a const object called " product " to  store information shown in the picture
+const product =  {
+    title : " Ball pen",
+    rating : 4 ,
+    offer : 5,
+    price : 270 
+};
+console.log (product ) ;
+// Create a const object called "profile " to store information shown in the picture 
+const profile = {
+    name : "Niyati Sharma" ,
+    post : 1965 ,
+    followers : 5690000 ,
+    following : 4 ,
+    isfollow : false ,
+    
+}
